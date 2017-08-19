f7644d5aa7edc0540bb5d09e3134d482f419dc98
dd <dds861@gmail.com>
2017-08-20 00:32:03 +0200

    some code changings

--- app/src/main/java/com/carfuelconsumption/dd/carfuelconsumption/MainActivity.java
+++ app/src/main/java/com/carfuelconsumption/dd/carfuelconsumption/MainActivity.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class MainActivity extends AppCompatActivity {
 
     android.app.FragmentTransaction fragmentTransaction;
@@ -16,11 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnCounting;
     Button btnInstructions;
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
 
         fragmentInstructions = new FragmentInstructions();
         fragmentCounting = new FragmentCounting();
